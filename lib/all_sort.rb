0844d6ef0bc8f80ec5f1705e257243e01992ba1a
jpcoderesearch <jpcoderesearch@gmail.com>
2018-09-09 19:11:07 +0530

    first commit

---
+++ lib/all_sort.rb
@@ -0,0 +1,43 @@
+require "all_sort/version"
+
+module AllSort
+ def bubble_sort arr
+ 	n =arr.length
+ 	loop do
+ 		swapped = false
+ 		(n-1).times do |i|
+ 			if arr[i] < array[i+1]
+ 				arr[i],arr[i+1] = arr[i+1],arr[i]
+ 				swapped = true
+ 			end
+ 		end
+ 		break if not swapped
+ 	end
+ 	arr
+ end
+ def quick_sort arr
+ 	return [] if arr.empty?
+ 	pivot = arr.delete_at(rand(size))
+ 	left, right = arr.partition(&pivot.method(:>))
+ 	return quick_sort(left)+pivot+quick_sort(right)
+ end
+ def merge_sort arr
+ 	return arr if arr.length <=1
+ 	piv = (arr.length/2)
+ 	r_array = merge_sort(arr[0..piv-1])
+ 	l_array = merge_sort(arr[piv..arr.length])
+ 	merger(r_array, l_array)
+ end
+ def merger right, left
+ 	if right.empty?
+ 		return left
+ 	elsif left.empty?
+ 		return right
+ 	elsif left.first < right.first
+ 		[left.first] + merger(left[1..left.length],right)
+ 	else
+ 		[right.first] + merger(left,right[1..right.length])
+ 	end
+ end
+end
+
---
+++ lib/all_sort/version.rb
@@ -0,0 +1,3 @@
+module AllSort
+  VERSION = "0.1.0"
+end
